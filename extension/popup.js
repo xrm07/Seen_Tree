8a2aedbd651cea32f71115381d23f5f0b9fe1aae
xrm07 <heyehema@gmail.com>
2025-09-04 13:41:15 +0900

    Remove extension directory

--- extension/popup.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('translate-page').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.tabs.sendMessage(tab.id, { type: 'START_PAGE_TRANSLATION' });
-});
